bfc33cfe9244e47c9597f3aa327945e718be418f
Miles <mham7714@gmail.com>
2020-05-10 16:25:14 -0400

    revising css to fit map in list form

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-let washington = document.querySelector('.washington')
-console.dir(washington)
 
-const washingtonUrl =  '/v4/mapbox.satellite/15/38.8971251/-77.0275864@2x.grid.json'
-S
+//washington
+var map;
+function initMap() {
+  map = new google.maps.Map(document.querySelector('#mapwash'), {
+    center: {lat: 38.9072, lng: 77.0369},
+    zoom: 11
+  });
+}
+
+// function initMap() {
+//     map = new google.maps.Map(document.querySelector('#mapny'), {
+//       center: {lat: 38.9072, lng: 77.0369},
+//       zoom: 11
+//     });
+//   }
+
+//   function initMap() {
+//     map = new google.maps.Map(document.querySelector('#mapmx'), {
+//       center: {lat: 38.9072, lng: 77.0369},
+//       zoom: 11
+//     });
+//   }
+
+// toggle menu bottons
+let buttons = document.querySelectorAll('button')
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+    let sibling = button.nextElementSibling
+if(sibling.style.display === 'flex') {
+    sibling.style.display = 'none'
+} else {
+    sibling.style.display = 'flex'
+}
+})
+});
+

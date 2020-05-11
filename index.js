1f9c125d3da85562861ec9e395a4f3957ac5893c
Miles <mham7714@gmail.com>
2020-05-11 15:58:39 -0400

    added footer

--- index.js
+++ index.js
@@ -4,15 +4,18 @@ var map;
 var map2;
 var map3
 function initMap() {
+    // Washington
   map = new google.maps.Map(document.querySelector('#mapwash'), {
     center: {lat: 38.89511, lng: -77.03637},
     zoom: 12
   })
+    // new york
   map = new google.maps.Map(document.querySelector('#mapny'), {
     center: {lat: 40.7128, lng: -74.0060},
     zoom: 12
   
 })
+    // mexico cito
 map = new google.maps.Map(document.querySelector('#mapmx'), {
     center: {lat: 19.4326, lng: -99.1332},
     zoom: 12
